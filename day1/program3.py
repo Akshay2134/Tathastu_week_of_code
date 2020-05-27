fbefac713921c5a194f204e17634b5f18c1b2f93
Akshay2134 <66027478+Akshay2134@users.noreply.github.com>
2020-05-27 23:03:11 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,8 @@
+x= int(input("Enter the value of x: "))
+y= int(input("Enter the value of y: "))
+x= x + y
+y= x - y
+x= x - y
+print("Swapping x and y")
+print("Value of x=", x)
+print("Value of y=", y)
